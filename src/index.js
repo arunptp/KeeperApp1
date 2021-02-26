0f0bef86b252dc7cd2f4e849d812bdd6f43a40db
arunptp <arunptp@gmail.com>
2021-02-26 10:21:09 +0530

    App Done

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "components/Ap.jsx";
+import App from "./components/App.jsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
